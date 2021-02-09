b184e252e72b8311f8e89814eb893e74fa48428a
Rosie <70326049+Caroline1609@users.noreply.github.com>
2021-02-09 16:24:35 +0100

    Update user.php

--- php/user.php
+++ php/user.php
@@ -56,9 +56,12 @@ if(isset($_GET['id'])){
     <h3>
         Mes Projets
     </h3>
+    <div class="card border-dark">
         <div class="row g-0">
+            <div class="col-md-6">
                 <img src="..." alt="...">
             </div>
+            <div class="col-md-6">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>

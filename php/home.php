e786a307845b484d322f78d97751bbb108ff3d6e
Rosie <70326049+Caroline1609@users.noreply.github.com>
2021-02-10 15:40:26 +0100

    mise a jour page principal

--- php/home.php
+++ php/home.php
@@ -11,9 +11,9 @@
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe width="100%" height="1080" class="embed-responsive-item" src="https://www.youtube.com/embed/COb6AqmggRU" allowfullscreen></iframe>
             </div>
-
-            <h4 id="promotion20-21">Promotion n°6 <br> 2020/2021</h4><br></br>
-            <div class="d-grid gap-2 d-md-block" style="margin-bottom:25px;">
+            <br></br>
+            <h2 id="promotion20-21">Promotion n°6 <br> 2020/2021</h4><br></br>
+            <div class="d-grid gap-2 d-md-block" style="margin-bottom:55px;">
                 <button Onclick="btn_crea()" id ="Crea" class="btn btn-primary btn-lg" type="button">Créa</button>
                 <button Onclick="btn_tech()" id="Tech" class="btn btn-primary btn-lg" type="button">Tech</button>
             </div>
